1e2ea698ff54a10d7c0bfd703779d6887fbb8f2b
blake <blake@3b8f3901-05c8-4ec7-8aa6-e4084bfc5a6f>
2011-04-03 21:57:45 +0000

    fix

--- src/GamePlay/__init__.py
+++ src/GamePlay/__init__.py
@@ -2,7 +2,6 @@ from . import playscene as _playscene
 from . import tile as _tile
 from . import level as _level
 from . import sprite as _sprite
-from . import citizen as _citizen
 
 Sprite = _sprite.Sprite
 Citizen = _sprite.Citizen
