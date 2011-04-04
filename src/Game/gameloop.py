683b14c73e573622502a44069896817fc3070e3d
blake <blake@3b8f3901-05c8-4ec7-8aa6-e4084bfc5a6f>
2011-04-04 19:51:36 +0000

    FPS counter

--- src/Game/gameloop.py
+++ src/Game/gameloop.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+import Resources
 
 class GameLoop:
     
@@ -11,6 +12,9 @@ class GameLoop:
         pygame.init()
         self.screen = pygame.display.set_mode((640, 480))
         self.quitting = False
+        self.show_fps_counter = True
+        self.lastNFrames = []
+        self.font = None
     
     def Start(self, startingScene):
         self.current = startingScene
@@ -19,22 +23,24 @@ class GameLoop:
     
     
     def DoLoop(self):
+        vfps = 0.0
+        afps = 0.0
         
         while not self.quitting:
             start = time.time()
-            
-            end = time.time()
             
             self.ProcessInput()
             
             self.Update()
             
-            self.Render()    
+            self.Render(vfps, afps)
             
             self.current = self.current.next
             
             if self.current == None:
                 self.quitting = True
+            
+            end = time.time()
             
             diff = end - start
             
@@ -42,6 +48,11 @@ class GameLoop:
             if diff < targetSPF:
                 delay = targetSPF - diff
                 time.sleep(delay)
+            
+            if self.show_fps_counter and diff > 0:
+                vfps = 1.0 / diff
+                afps = 1.0 / (time.time() - start)
+                    
     
     def ProcessInput(self):
         
@@ -61,9 +72,15 @@ class GameLoop:
         if self.current != None:
             self.current.Update()
         
-    def Render(self):
+    def Render(self, vfps, afps):
         if self.current != None:
             self.current.Render(self.screen)
+            
+            if self.show_fps_counter:
+                if self.font == None:
+                    self.font = Resources.Font((255, 255, 255))
+                text = self.font.Render("FPS: " + str(vfps))
+                self.screen.blit(text, (4, 480 - text.get_height()))
             pygame.display.flip()
     
     
