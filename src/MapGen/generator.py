f81c40d8493f0aa372f7a4f804395d163e426cc6
blake <blake@3b8f3901-05c8-4ec7-8aa6-e4084bfc5a6f>
2011-04-05 21:18:40 +0000

    random buildings

--- src/MapGen/generator.py
+++ src/MapGen/generator.py
@@ -29,21 +29,31 @@ class Generator:
 			self.tasks[0]()
 			self.tasks = self.tasks[1:]
 	
+	def RandomBlocks(self, num):
+		output = []
+		width = self.logGridWidth
+		height = self.logGridHeight
+		while num > 0:
+			x = random.randint(0, width - 1)
+			y = random.randint(0, height - 1)
+			output.append((x, y))
+			num -= 1
+		return output 
+	
 	def CompoundLogicalGrid(self):
 		grid = self.logicalGrid
 		
 		width = self.logGridWidth
 		height = self.logGridHeight
 		
-		compounds = width * height // 2
+		compounds = self.RandomBlocks(width * height // 2)
 		
-		i = 0
-		while i < compounds:
+		for compound in compounds:
 			
-			x = random.randint(0, width - 1)
-			y = random.randint(0, height - 1)
+			x = compound[0]
+			y = compound[1]
 			direction = random.randint(0, 3)
-			
+			print x, y, direction
 			if not grid[x][y][direction]:
 				grid[x][y][direction] = True
 				if direction == 0 and x > 0:
@@ -55,17 +65,52 @@ class Generator:
 				elif direction == 3 and y < height - 1:
 					grid[x][y + 1][1] = True
 		
-			
-			i += 1
-		
 		self.tasks.append(self.GenerateBlockList)
 	
 	def GenerateBlockList(self):
-		# TODO: this
+		
+		
+		
 		self.tasks.append(self.PopulateBlocks)
 	
 	def PopulateBlocks(self):
-		# TODO: this
+		commands = []
+		grid = self.logicalGrid
+		longWidth = self.cellWidth + 7
+		for block in self.RandomBlocks(self.logGridHeight * self.logGridWidth // 2):
+			x = block[0]
+			y = block[1]
+			cell = grid[x][y]
+			if not cell[4]:
+				left = (x - 1) * longWidth + 12
+				top = (y - 1) * longWidth + 12
+				width = longWidth - 7
+				height = longWidth - 7
+				roofHeight = 3
+				if not (cell[0] or cell[1] or cell[2] or cell[3]):
+					pass
+				else:
+					if cell[0] and not grid[x - 1][y][4]:
+						left -= longWidth
+						width += longWidth
+						grid[x - 1][y][4] = True
+					elif cell[1] and not grid[x][y - 1][4]:
+						top -= longWidth
+						height += longWidth
+						grid[x][y - 1][4] = True
+						roofHeight += longWidth
+					elif cell[2] and not grid[x + 1][y][4]:
+						width += longWidth
+						grid[x + 1][y][4] = True
+					elif cell[3] and not grid[x][y + 1][4]:
+						height += longWidth
+						grid[x][y + 1][4] = True
+						roofHeight += longWidth
+					if random.random() < .5:
+						roofHeight += 2
+				commands.append('BUILDING %d %d %d %d %d 1' % (left, top, width, height, roofHeight))
+				
+		self.commands += commands
 		self.tasks.append(self.SerializeStreets)
 	
 	def SerializeStreets(self):
