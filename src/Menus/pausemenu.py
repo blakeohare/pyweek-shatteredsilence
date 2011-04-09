ac46e9d8a8cd7a42ce50c24eb77b3be0da37d64f
falun <falun@3b8f3901-05c8-4ec7-8aa6-e4084bfc5a6f>
2011-04-09 10:07:32 +0000

    pause menu

--- src/Menus/pausemenu.py
+++ src/Menus/pausemenu.py
@@ -1,20 +1,43 @@
 import pygame
 import Menus
 import Game
+import Resources
 
+RESUME = 1
+TITLE = 2
 
 class GamePauseMenu(Game.GameSceneBase):
 	
 	def __init__(self, currentScene):
 		Game.GameSceneBase.__init__(self)
 		self.previousScene = currentScene
+		self._font = Resources.TTF_Font('Kallamar/KALLAMAR.TTF')
 		self.bg = pygame.Surface((640, 480))
 		self.bg.set_alpha(50)
+		
+		self._resume_r = pygame.Rect(0,0,0,0)
+		self._title_r = pygame.Rect(0,0,0,0)
+		self._hover = None
 	
 	def ProcessInput(self, events):
-		
-		
-		pass
+		for e in events:
+			if e.type == pygame.MOUSEMOTION:
+				self._hover = None
+				x = e.pos[0]
+				y = e.pos[1]
+				if self._resume_r.collidepoint(x,y):
+					self._hover = RESUME
+				elif self._title_r.collidepoint(x,y):
+					self._hover = TITLE
+			if e.type == pygame.MOUSEBUTTONUP and e.button == 1:
+				x = e.pos[0]
+				y = e.pos[1]
+				if self._resume_r.collidepoint(x,y):
+					self.ReturnToGame()
+				elif self._title_r.collidepoint(x,y):
+					self.ReturnToMainMenu()
+			if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+				self.ReturnToGame()
 	
 	def Update(self):
 		pass
@@ -31,5 +54,16 @@ class GamePauseMenu(Game.GameSceneBase):
 		self.previousScene.Render(self.bg)
 		screen.blit(self.bg, (0, 0))
 		
-		# draw two mouse-clickable options: "Return to game" and "Exit to Main Menu"
-		
+		f = self._font.Render
+		basic = pygame.Color(255, 255, 255)
+		hover = pygame.Color(0, 110, 180)
+		t = f('Resume Game', (basic, hover)[self._hover == RESUME])
+		x = (640-t.get_width()) / 2
+		y = 220
+		screen.blit(t, (x, y))
+		self._resume_r = pygame.Rect(x, y, t.get_width(), t.get_height())
+		t = f('Exit To Main Menu', (basic, hover)[self._hover == TITLE])
+		x = (640-t.get_width()) / 2
+		y = 220 + 2 * t.get_height()
+		screen.blit(t, (x, y))
+		self._title_r = pygame.Rect(x, y, t.get_width(), t.get_height())
